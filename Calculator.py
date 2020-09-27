e8d3534397442ba8269665d1642468224750051f
Niramay33 <70151582+Niramay33@users.noreply.github.com>
2020-09-27 19:43:40 +0530

    Update and rename README.md to Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,29 @@
+# A calculator made in PYTHON  
+
+print("CALCULATOR")
+print("ADD")
+print("SUB")
+print("MUL")
+print("DIV")
+
+i = int(input("Enter number 1 for add"))
+if i == 1:
+    a = int(input("Enter A:"))
+    b = int(input("Enter B:"))
+    c = a + b
+    print("Sum = ", c)
+if i == 2:
+    a = int(input("Enter A:"))
+    b = int(input("Enter B:"))
+    c = a - b
+    print("Sum = ", c)
+if i == 3:
+    a = int(input("Enter A:"))
+    b = int(input("Enter B:"))
+    c = a * b
+    print("Sum = ", c)
+if i == 4:
+    a = int(input("Enter A:"))
+    b = int(input("Enter B:"))
+    c = a + b
+    print("Sum = ", c)
