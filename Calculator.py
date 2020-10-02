ee1a4740c442a1dcfa8e554357e30991b232ea8d
Niramay33 <70151582+Niramay33@users.noreply.github.com>
2020-10-02 19:35:14 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,29 +1,29 @@
 # A calculator made in PYTHON  
 
 print("CALCULATOR")
-print("ADD")
-print("SUB")
-print("MUL")
-print("DIV")
+print("1 to ADD")
+print("2 to SUB")
+print("3 to MUL")
+print("4 to DIV")
 
 i = int(input("Enter number :- "))
 if i == 1:
-    a = int(input("Enter A:"))
-    b = int(input("Enter B:"))
+    a = int(input("Enter 1st digit :- "))
+    b = int(input("Enter 2nd digit :- "))
     c = a + b
     print("Sum = ", c)
 if i == 2:
-    a = int(input("Enter A:"))
-    b = int(input("Enter B:"))
+    a = int(input("Enter 1st digit :- "))
+    b = int(input("Enter 2nd digit :- "))
     c = a - b
     print("Sum = ", c)
 if i == 3:
-    a = int(input("Enter A:"))
-    b = int(input("Enter B:"))
+    a = int(input("Enter 1st digit :- "))
+    b = int(input("Enter 2nd digit :- "))
     c = a * b
     print("Sum = ", c)
 if i == 4:
-    a = int(input("Enter A:"))
-    b = int(input("Enter B:"))
+    a = int(input("Enter 1st digit :- "))
+    b = int(input("Enter 2nd digit :- "))
     c = a + b
     print("Sum = ", c)
